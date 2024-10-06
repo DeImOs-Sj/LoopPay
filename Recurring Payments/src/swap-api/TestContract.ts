f247dda5365af68e741659abc860d5e77a5bab47
Shlok Main <shlokjagtap123@gmail.com>
2024-10-06 23:36:39 +0530

    errors in contract fixed

--- Recurring Payments/src/swap-api/TestContract.ts
+++ Recurring Payments/src/swap-api/TestContract.ts
@@ -18,7 +18,8 @@ import type {
   BN,
   FunctionFragment,
   InvokeFunction,
-} from 'fuels';
+  StrSlice,
+} from "fuels";
 
 export type AddressInput = { bits: string };
 export type AddressOutput = AddressInput;
@@ -26,128 +27,193 @@ export type AssetIdInput = { bits: string };
 export type AssetIdOutput = AssetIdInput;
 
 const abi = {
-  "programType": "contract",
-  "specVersion": "1",
-  "encodingVersion": "1",
-  "concreteTypes": [
+  programType: "contract",
+  specVersion: "1",
+  encodingVersion: "1",
+  concreteTypes: [
     {
-      "type": "()",
-      "concreteTypeId": "2e38e77b22c314a449e91fafed92a43826ac6aa403ae6a8acb6cf58239fbaf5d"
+      type: "()",
+      concreteTypeId:
+        "2e38e77b22c314a449e91fafed92a43826ac6aa403ae6a8acb6cf58239fbaf5d",
     },
     {
-      "type": "struct std::address::Address",
-      "concreteTypeId": "f597b637c3b0f588fb8d7086c6f4735caa3122b85f0423b82e489f9bb58e2308",
-      "metadataTypeId": 1
+      type: "bool",
+      concreteTypeId:
+        "b760f44fa5965c2474a3b471467a22c43185152129295af588b022ae50b50903",
     },
     {
-      "type": "struct std::asset_id::AssetId",
-      "concreteTypeId": "c0710b6731b1dd59799cf6bef33eee3b3b04a2e40e80a0724090215bbf2ca974",
-      "metadataTypeId": 2
+      type: "str",
+      concreteTypeId:
+        "8c25cb3686462e9a86d2883c5688a22fe738b0bbc85f458d2d2b5f3f667c6d5a",
     },
     {
-      "type": "u64",
-      "concreteTypeId": "1506e6f44c1d6291cdf46395a8e573276a4fa79e8ace3fc891e092ef32d1b0a0"
-    }
+      type: "struct std::address::Address",
+      concreteTypeId:
+        "f597b637c3b0f588fb8d7086c6f4735caa3122b85f0423b82e489f9bb58e2308",
+      metadataTypeId: 1,
+    },
+    {
+      type: "struct std::asset_id::AssetId",
+      concreteTypeId:
+        "c0710b6731b1dd59799cf6bef33eee3b3b04a2e40e80a0724090215bbf2ca974",
+      metadataTypeId: 2,
+    },
+    {
+      type: "u64",
+      concreteTypeId:
+        "1506e6f44c1d6291cdf46395a8e573276a4fa79e8ace3fc891e092ef32d1b0a0",
+    },
   ],
-  "metadataTypes": [
+  metadataTypes: [
     {
-      "type": "b256",
-      "metadataTypeId": 0
+      type: "b256",
+      metadataTypeId: 0,
     },
     {
-      "type": "struct std::address::Address",
-      "metadataTypeId": 1,
-      "components": [
+      type: "struct std::address::Address",
+      metadataTypeId: 1,
+      components: [
         {
-          "name": "bits",
-          "typeId": 0
-        }
-      ]
-    },
-    {
-      "type": "struct std::asset_id::AssetId",
-      "metadataTypeId": 2,
-      "components": [
-        {
-          "name": "bits",
-          "typeId": 0
-        }
-      ]
-    }
-  ],
-  "functions": [
-    {
-      "inputs": [],
-      "name": "deposit_funds",
-      "output": "2e38e77b22c314a449e91fafed92a43826ac6aa403ae6a8acb6cf58239fbaf5d",
-      "attributes": [
-        {
-          "name": "storage",
-          "arguments": [
-            "write"
-          ]
+          name: "bits",
+          typeId: 0,
         },
-        {
-          "name": "payable",
-          "arguments": []
-        }
-      ]
-    },
-    {
-      "inputs": [],
-      "name": "get_contract_balance",
-      "output": "1506e6f44c1d6291cdf46395a8e573276a4fa79e8ace3fc891e092ef32d1b0a0",
-      "attributes": [
-        {
-          "name": "storage",
-          "arguments": [
-            "read"
-          ]
-        }
-      ]
-    },
-    {
-      "inputs": [
-        {
-          "name": "recipient",
-          "concreteTypeId": "f597b637c3b0f588fb8d7086c6f4735caa3122b85f0423b82e489f9bb58e2308"
-        },
-        {
-          "name": "asset_id",
-          "concreteTypeId": "c0710b6731b1dd59799cf6bef33eee3b3b04a2e40e80a0724090215bbf2ca974"
-        },
-        {
-          "name": "amount",
-          "concreteTypeId": "1506e6f44c1d6291cdf46395a8e573276a4fa79e8ace3fc891e092ef32d1b0a0"
-        }
       ],
-      "name": "transfer",
-      "output": "2e38e77b22c314a449e91fafed92a43826ac6aa403ae6a8acb6cf58239fbaf5d",
-      "attributes": [
+    },
+    {
+      type: "struct std::asset_id::AssetId",
+      metadataTypeId: 2,
+      components: [
         {
-          "name": "payable",
-          "arguments": []
+          name: "bits",
+          typeId: 0,
+        },
+      ],
+    },
+  ],
+  functions: [
+    {
+      inputs: [],
+      name: "deposit_funds",
+      output:
+        "2e38e77b22c314a449e91fafed92a43826ac6aa403ae6a8acb6cf58239fbaf5d",
+      attributes: [
+        {
+          name: "storage",
+          arguments: ["write"],
         },
         {
-          "name": "storage",
-          "arguments": [
-            "read",
-            "write"
-          ]
-        }
-      ]
-    }
+          name: "payable",
+          arguments: [],
+        },
+      ],
+    },
+    {
+      inputs: [],
+      name: "get_contract_balance",
+      output:
+        "1506e6f44c1d6291cdf46395a8e573276a4fa79e8ace3fc891e092ef32d1b0a0",
+      attributes: [
+        {
+          name: "storage",
+          arguments: ["read"],
+        },
+      ],
+    },
+    {
+      inputs: [
+        {
+          name: "payment_id",
+          concreteTypeId:
+            "1506e6f44c1d6291cdf46395a8e573276a4fa79e8ace3fc891e092ef32d1b0a0",
+        },
+      ],
+      name: "process_recurring_payment",
+      output:
+        "b760f44fa5965c2474a3b471467a22c43185152129295af588b022ae50b50903",
+      attributes: [
+        {
+          name: "storage",
+          arguments: ["read", "write"],
+        },
+      ],
+    },
+    {
+      inputs: [
+        {
+          name: "recipient",
+          concreteTypeId:
+            "f597b637c3b0f588fb8d7086c6f4735caa3122b85f0423b82e489f9bb58e2308",
+        },
+        {
+          name: "amount",
+          concreteTypeId:
+            "1506e6f44c1d6291cdf46395a8e573276a4fa79e8ace3fc891e092ef32d1b0a0",
+        },
+        {
+          name: "interval",
+          concreteTypeId:
+            "1506e6f44c1d6291cdf46395a8e573276a4fa79e8ace3fc891e092ef32d1b0a0",
+        },
+      ],
+      name: "setup_recurring_payment",
+      output:
+        "2e38e77b22c314a449e91fafed92a43826ac6aa403ae6a8acb6cf58239fbaf5d",
+      attributes: [
+        {
+          name: "storage",
+          arguments: ["write"],
+        },
+      ],
+    },
+    {
+      inputs: [
+        {
+          name: "recipient",
+          concreteTypeId:
+            "f597b637c3b0f588fb8d7086c6f4735caa3122b85f0423b82e489f9bb58e2308",
+        },
+        {
+          name: "asset_id",
+          concreteTypeId:
+            "c0710b6731b1dd59799cf6bef33eee3b3b04a2e40e80a0724090215bbf2ca974",
+        },
+        {
+          name: "amount",
+          concreteTypeId:
+            "1506e6f44c1d6291cdf46395a8e573276a4fa79e8ace3fc891e092ef32d1b0a0",
+        },
+      ],
+      name: "transfer",
+      output:
+        "2e38e77b22c314a449e91fafed92a43826ac6aa403ae6a8acb6cf58239fbaf5d",
+      attributes: [
+        {
+          name: "payable",
+          arguments: [],
+        },
+        {
+          name: "storage",
+          arguments: ["read", "write"],
+        },
+      ],
+    },
   ],
-  "loggedTypes": [],
-  "messagesTypes": [],
-  "configurables": []
+  loggedTypes: [
+    {
+      logId: "10098701174489624218",
+      concreteTypeId:
+        "8c25cb3686462e9a86d2883c5688a22fe738b0bbc85f458d2d2b5f3f667c6d5a",
+    },
+  ],
+  messagesTypes: [],
+  configurables: [],
 };
 
 const storageSlots: StorageSlot[] = [
   {
-    "key": "63a60e5d8a026ee74ad811c1d1c1a301e57d0baa431b3796e1628f4219d08948",
-    "value": "0000000000000000000000000000000000000000000000000000000000000000"
-  }
+    key: "63a60e5d8a026ee74ad811c1d1c1a301e57d0baa431b3796e1628f4219d08948",
+    value: "0000000000000000000000000000000000000000000000000000000000000000",
+  },
 ];
 
 export class TestContractInterface extends Interface {
@@ -158,6 +224,8 @@ export class TestContractInterface extends Interface {
   declare functions: {
     deposit_funds: FunctionFragment;
     get_contract_balance: FunctionFragment;
+    process_recurring_payment: FunctionFragment;
+    setup_recurring_payment: FunctionFragment;
     transfer: FunctionFragment;
   };
 }
@@ -170,12 +238,23 @@ export class TestContract extends Contract {
   declare functions: {
     deposit_funds: InvokeFunction<[], void>;
     get_contract_balance: InvokeFunction<[], BN>;
-    transfer: InvokeFunction<[recipient: AddressInput, asset_id: AssetIdInput, amount: BigNumberish], void>;
+    process_recurring_payment: InvokeFunction<
+      [payment_id: BigNumberish],
+      boolean
+    >;
+    setup_recurring_payment: InvokeFunction<
+      [recipient: AddressInput, amount: BigNumberish, interval: BigNumberish],
+      void
+    >;
+    transfer: InvokeFunction<
+      [recipient: AddressInput, asset_id: AssetIdInput, amount: BigNumberish],
+      void
+    >;
   };
 
   constructor(
     id: string | AbstractAddress,
-    accountOrProvider: Account | Provider,
+    accountOrProvider: Account | Provider
   ) {
     super(id, abi, accountOrProvider);
   }
